b2e8922dc8e25846fa07a844f92e3b60117a4fd8
Khushipandey-456 <kp9761560@gmail.com>
2024-12-08 11:38:19 +0530

    Features/Login

--- ques1.js
+++ ques1.js
@@ -9,5 +9,19 @@ function signUp(str){
         console.log("Already Registered......,Please Login..,")
 
     }
-    
-} console.log(signUp("Sham"))
+} console.log(signUp("Alice"))
+
+// features/login
+function login(userName,passWord){
+ let employeesName=["Alice","Bob","Charlie"];
+ if (employeesName.includes(userName)) {
+ if(passWord="Emp@123"){
+    console.log("Login Successfull...")
+ }else {
+    return "Wrong Password...";
+}
+} 
+else {
+return "User Not Found, Please Signup";
+}
+} console.log(login("Rossy","Emp@123"))
